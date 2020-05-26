3002ded04033439e8f18468acb4dc8567fd4536b
Don <Don@DESKTOP-6CP2B64>
2020-05-26 20:46:24 +0900

    feat: Add solves

---
+++ BaekJoonAlgorithm/src/problem/Q10039.java
@@ -0,0 +1,21 @@
+package problem;
+
+import java.util.Scanner;
+
+public class Q10039 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int A = sc.nextInt();
+			if(A < 40) {
+				A = 40;
+			}
+			sum += A;
+		}
+		System.out.println(sum / 5);
+	}
+
+}
--- BaekJoonAlgorithm/src/problem/Q10952.java
+++ BaekJoonAlgorithm/src/problem/Q10952.java
@@ -1,9 +1,27 @@
 package problem;
 
+import java.util.Scanner;
+
 public class Q10952 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		// 조건식에 A, B는 0 초과기 때문에 0만 입력받아도 멈추면 됌.
+		// sc.nexInt() 사용시 첫 입력을 넘어감.
+		// break 문 사용이나 blean flag 사용 가능.
+		while(true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int sum = A + B;
+
+			if(A == 0 && B == 0) {
+				break;
+			}
+			
+			System.out.println(sum);
+			
+		}
 	}
 
 }
