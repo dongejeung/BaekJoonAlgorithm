40e9fae95e47641289c7bbc6e706a500d1d5df02
Don <Don@DESKTOP-6CP2B64>
2020-05-29 17:22:17 +0900

    test

--- BaekJoonAlgorithm/src/problem/step/six/array/Q8958.java
+++ BaekJoonAlgorithm/src/problem/step/six/array/Q8958.java
@@ -1,9 +1,12 @@
 package problem.step.six.array;
 
+import java.util.Scanner;
+
 public class Q8958 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
