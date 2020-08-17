7df6f2ac4aaf0581d87e23baf2e17c50753c4157
Don <Don@DESKTOP-6CP2B64>
2020-08-18 08:58:37 +0900

    add some

--- BaekJoonAlgorithm/src/problem/step/nine/math1/Q1011.java
+++ BaekJoonAlgorithm/src/problem/step/nine/math1/Q1011.java
@@ -1,22 +1,38 @@
 package problem.step.nine.math1;
 
 import java.util.Scanner;
-
+/*
+ * 아마 대칭의 형태를 띄고 있을 것으로 예측
+ * 거리가 홀수일 경우 짝수일 경우를 나눠야 할 듯
+ * 
+ * =====
+ * 
+ * => 점화식을 함수로 표현?
+ * 
+ */
 public class Q1011 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// 테스트 케이스 수
 		int T = sc.nextInt();
 		
-		int[][] cases = new int[T][2];
+		int[] cases = new int[T];
 		
 		for (int i = 0; i < cases.length; i++) {
-			// 시작지점
-			cases[i][0] = sc.nextInt();
-			// 도착지점
-			cases[i][1] = sc.nextInt();
+			int distance = 0;
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			
+			distance = end - start;
 			
 			
 		}
+	}
+	
+	public static int nextDistance(int pre) {
+		
+		int next = pre;
+		
+		return 0;
 	}
 }
