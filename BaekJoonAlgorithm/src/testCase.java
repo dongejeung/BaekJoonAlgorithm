f327b1f5c085114dfcd5f8bf1b4b336fe3fd59d0
DON <DON@175.197.5.207>
2021-03-03 07:35:18 +0900

    add somes

--- BaekJoonAlgorithm/src/testCase.java
+++ BaekJoonAlgorithm/src/testCase.java
@@ -17,5 +17,11 @@ public class testCase {
 		
 		System.out.println(N / K);
 		
+		float f = 9.1234567f;
+		int i = Float.floatToIntBits(f);
+		
+		System.out.printf("%f%n", f);
+		System.out.printf("%X%n", i);
+		
 	}
 }
