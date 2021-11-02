52f8e29a4ffcb9abd002baf49bd94869994b4800
DON <DON@175.197.5.207>
2021-11-02 23:01:09 +0900

    add somes

--- BaekJoonAlgorithm/src/problem/code/plus/Q14500.java
+++ BaekJoonAlgorithm/src/problem/code/plus/Q14500.java
@@ -2,14 +2,31 @@ package problem.code.plus;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Q14500 {
-	static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stk.nextToken());
+		int M = Integer.parseInt(stk.nextToken());
 		
+		int[][] tet = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer stk2 = new StringTokenizer(br.readLine()); 
+			for (int j = 0; j < M; j++) {
+				tet[i][j] = Integer.parseInt(stk2.nextToken());
+				bw.write(Integer.toString(tet[i][j])+" ");
+			}
+			bw.write("\n");
+		}
 		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
